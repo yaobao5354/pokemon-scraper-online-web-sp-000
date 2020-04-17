5ae0b94b267cd1217396c4ec7b53de604d58d7ca
yaoyu bao <yaobao5354@gmail.com>
2020-04-17 16:44:06 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -27,12 +27,21 @@ class Pokemon
      LIMIT 1
     SQL
   row = db.execute(sql)
+<<<<<<< HEAD
   attribute = {
     :id => row[0][0] ,
     :name => row [0][1],
     :type => row [0][2]
   }
   Pokemon.new(attribute)
+=======
+  attribute = {}
+    attribute [:id] = row[0][0]
+    attribute [:name] = row [0][1]
+    attribute [:type] = row [0][2]
+    attribute [:db] = db
+  self.new(attribute)
+>>>>>>> 7cfa1e7dcf4ac5dc8a3916cac15eedee6491b608
   end
 
 end
